cf9092343c9815dec0cb0a303297de68679cd25e
MattHughesEE <matt.hughes.ee@gmail.com>
2021-06-17 07:18:36 -0500

    migrations yaml

---
+++ .semaphore/migrations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+S3_PUBLISH_BUCKET="semaphore-test-app"
+
+file_version=$1
+stack_name=$2
+
+file_name=migrations-$file_version.zip
+
+cache restore gems-$SEMAPHORE_GIT_BRANCH-$(checksum Gemfile.lock),gems-develop,gems-master
+sem-version ruby 2.6.3
+gem uninstall bundler --all --executables
+gem install bundler --version 2.0.2 --no-document
+bundle install --local --deployment --jobs=2 --path=vendor/bundle
+sem-service start postgres
+RAILS_ENV=production bundle exec rake db:create db:structure:load
+RAILS_ENV=production bundle exec rake assets:precompile
+
+zip $file_name -9 -y -r . -x "spec/*" "tmp/*" "vendor/bundle/*" ".git/*"
+
+aws s3 --profile sem-ci-service cp $file_name s3://$S3_PUBLISH_BUCKET/migrations/$file_name
+
+parameters=`aws cloudformation describe-stacks --profile sem-ci-service --stack-name $stack_name | jq -r '.Stacks[].Parameters[].ParameterKey | select( . != "BundleKey")'`
+
+echo "[" > params.json
+for parameter in $parameters; do
+  echo "{\"ParameterKey\": \"$parameter\", \"UsePreviousValue\": true}," >> params.json
+done
+echo "{\"ParameterKey\": \"BundleKey\", \"ParameterValue\": \"migrations/$file_name\"}" >> params.json
+echo "]" >> params.json
+
+aws cloudformation update-stack --profile sem-ci-service --stack-name $stack_name --use-previous-template --parameters file://params.json
