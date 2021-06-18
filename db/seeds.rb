cf366c344db9102e798d926b0a6be6f6a9d50d73
MattHughesEE <matt.hughes.ee@gmail.com>
2021-06-18 09:34:08 -0500

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the Rails db:seed command (or created alongside the database with db:setup).
+movies = Movie.create([
+    { name: 'Star Wars', year: 1977, genre: 'SCI-FI'}, 
+    { name: 'Lord of the Rings', year: 2001, genre: 'Fantasy' }
+])
