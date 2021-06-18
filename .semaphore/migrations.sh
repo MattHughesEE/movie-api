41a4de322f233db627e7b95de87de69a886749bb
MattHughesEE <matt.hughes.ee@gmail.com>
2021-06-18 11:58:06 -0500

    TemplateURL

--- .semaphore/migrations.sh
+++ .semaphore/migrations.sh
@@ -31,4 +31,4 @@ done
 echo "{\"ParameterKey\": \"BundleKey\", \"ParameterValue\": \"migrations/$file_name\"}" >> params.json
 echo "]" >> params.json
 
-aws cloudformation create-stack --region us-east-1 --stack-name $stack_name --parameters file://params.json
+aws cloudformation create-stack --region us-east-1 --stack-name $stack_name --templateURL arn:aws:cloudformation:us-east-1:673626449192:stack/awseb-e-3hxfcaypcj-stack/5d8ce470-ce8c-11eb-9020-0a8c479d0b73 --parameters file://params.json
